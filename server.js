cb4d43453d78f6c8ee00dd4831dba6e0aabc9bba
Hulunlante Worku <hulunlante.w@gmail.com>
2022-01-31 22:49:59 +0300

    Work Breakdown is done

---
+++ server.js
@@ -0,0 +1,21 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const fileUpload = require('express-fileupload')
+const cookieParser = require('cookie-parser')
+const path = require('path')
+
+const app = express()
+app.use(express.json())
+app.use(cookieParser())
+app.use(cors())
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+const PORT = process.env.PORT 
+
+app.listen(PORT, () =>{
+    console.log('🚀 Server is running on port', PORT)
+})
