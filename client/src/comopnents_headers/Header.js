2bf4b35c4505a4b570f3985f06ca4346c9c7d6a7
Gashaw <kidanugashaw@gmail.com>
2022-02-01 09:12:40 -0800

    layout headers content and admin validation

--- client/src/comopnents_headers/Header.js
+++ client/src/comopnents_headers/Header.js
@@ -46,7 +46,40 @@ function Header() {
 
     return (
         <header>
-           
+            <div className="menu" onClick={() => setMenu(!menu)}>
+                <img src={Menu} alt="" width="30" />
+            </div>
+
+            <div className="logo">
+                <h1>
+                    <Link to="/">{isAdmin ? 'Admin' : 'DevAT Shop'}</Link>
+                </h1>
+            </div>
+
+            <ul style={styleMenu}>
+                <li><Link to="/">{isAdmin ? 'Products' : 'Shop'}</Link></li>
+
+                {isAdmin && adminRouter()}
+
+                {
+                    isLogged ? loggedRouter() : <li><Link to="/login">Login ✥ Register</Link></li>
+                }
+
+                <li onClick={() => setMenu(!menu)}>
+                    <img src={Close} alt="" width="30" className="menu" />
+                </li>
+
+            </ul>
+
+            {
+                isAdmin ? '' 
+                :<div className="cart-icon">
+                    <span>{cart.length}</span>
+                    <Link to="/cart">
+                        <img src={Cart} alt="" width="30" />
+                    </Link>
+                </div>
+            }
             
         </header>
     )
