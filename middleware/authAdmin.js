b7903eed2c8316e556e51716ae76dd4090bc8cc8
Enoch <henokwon@gmail.com>
2022-02-05 13:57:36 -0800

    description modified

--- middleware/authAdmin.js
+++ middleware/authAdmin.js
@@ -1,21 +1,18 @@
-const Users = require('../models/userModel')
+import { findOne } from "../models/userModel";
 
-const authAdmin = async (req, res, next) =>{
-    try {
-        // Get user information by id
-        const user = await Users.findOne({
-            _id: req.user.id
-        })
-       // During Schema Fields role 0 - [ Users ]  role 1- [ Admin ]
-        if(user.role === 0)
-            return res.status(400).json({msg: "Admin resources access denied"})
+const authAdmin = async (req, res, next) => {
+  try {
+    // Get user information by id
+    const user = await findOne({
+      _id: req.user.id,
+    });
+    // Schema Fields role 0 is assigned for Users and role 1 is assigned for admin
+    if (user.role === 0)
+      return res.status(400).json({ msg: "Admin resources access denied" });
 
-        next()
-        
-    } catch (err) {
-        return res.status(500).json({msg: err.message})
-    }
-}
-
-module.exports = authAdmin
-
+    next();
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+};
+export default authAdmin;
